853c2c1564c4d5c7a4465d128122c61735e5b11f
Mary Benitez <meebenitez@gmail.com>
2017-10-22 03:49:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,13 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  
+
 
 
 end
